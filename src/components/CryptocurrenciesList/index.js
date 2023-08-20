3898184e97841065cdd0b95e3a44ec3ae44b8cc8
“Jagadeesh-176” <jagadeeshalamanda176@gmail.com>
2023-08-20 06:22:52 +0000

    “solution”

---
+++ src/components/CryptocurrenciesList/index.js
@@ -0,0 +1,49 @@
+// Write your JS code here
+import {Component} from 'react'
+import CryptocurrencyItem from '../CryptocurrencyItem'
+import './index.css'
+
+class CryptocurrenciesList extends Component {
+  cryptocurrencyHeader = () => (
+    <div className="list-header">
+      <p className="list-coin-type-heading">Coin Type</p>
+      <div className="usd-and-euro-values-container">
+        <p className="list-coin-value-heading">USD</p>
+        <p className="list-coin-value-heading">EURO</p>
+      </div>
+    </div>
+  )
+
+  renderCryptocurrenciesView = () => {
+    const {cryptocurrenciesData} = this.props
+    return (
+      <div className="list-con">
+        {this.cryptocurrencyHeader()}
+        <ul className="list">
+          {cryptocurrenciesData.map(eachData => (
+            <CryptocurrencyItem
+              key={eachData.id}
+              cryptocurrencyDetails={eachData}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="cryptocurrencies-container">
+        <h1 className="heading">Cryptocurrency Tracker</h1>
+        <img
+          className="cryptocurrency-img"
+          src="https://assets.ccbp.in/frontend/react-js/cryptocurrency-bg.png"
+          alt="cryptocurrency"
+        />
+        {this.renderCryptocurrenciesView()}
+      </div>
+    )
+  }
+}
+
+export default CryptocurrenciesList
